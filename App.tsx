9d7724938d2433268cd31c012f6738d039dab131
Agustin Caraballo <52040470+MrAgus13@users.noreply.github.com>
2025-02-13 11:44:13 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Welcome to My Awesome App!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
+
+export default App; 
