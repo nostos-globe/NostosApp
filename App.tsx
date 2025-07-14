18ec97db49c89371d9e7411159a1b7192a40f4dc
Agustin <dyagus1329@gmail.com>
2025-07-14 21:27:10 +0200

    Add a Welcome Screen to the App

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -32,6 +33,7 @@ function App(): JSX.Element {
     return (
         <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="CreateProfile" component={CreateProfileScreen} />
